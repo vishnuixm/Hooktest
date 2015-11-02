67de0ff972e07ed50e91ddac08d24d7bf45df0ec
Vishnu Prasad <vishnuprasad.s@pandaweb.in>
2015-11-02 20:47:05 +0530

    initcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hooktest::Application.config.secret_token = '51973cb97971ba99fe20a16036b3579b920ad22126f1c3b46e9d3683e3339dc3107aad752454d91ed915afad1b4a21da4b4ab553245572c932469f2f1d95ef01'
